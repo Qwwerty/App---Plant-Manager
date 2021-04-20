17891a19110ad5628e9289b3eca2dd08012d84fb
DESKTOP-V5NHMSB\Rhalf <verdinhoraro2009@hotmail.com>
2021-04-20 11:35:39 -0300

    refactor: alterar css da página bem-vindo

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,44 +1,51 @@
 import React from 'react';
-import { 
-  SafeAreaView, 
-  Text, 
-  Image, 
-  StyleSheet, 
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  StyleSheet,
   TouchableOpacity,
-  Dimensions 
+  Dimensions,
+  View
 } from 'react-native';
+
+import { Feather } from '@expo/vector-icons'
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.title}>
-        Gerencie {'\n'}
-        suas plantas{'\n'} 
-        de forma fácil
-      </Text>
-
-      <Image 
-        source={wateringImg} 
-        style={styles.image} 
-        resizeMode="contain"
-      />
-
-      <Text style={styles.subtitle}>
-        Não esqueça mais de regar suas plantas.
-        Nós cuidamos de lembrar você sempre que precisar.
-      </Text>
-
-      <TouchableOpacity
-        style={styles.button}
-        activeOpacity={0.7}
-      >
-        <Text style={styles.buttonText}>
-          >
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>
+          Gerencie {'\n'}
+          suas plantas de {'\n'}
+          forma fácil
         </Text>
-      </TouchableOpacity>
+
+        <Image
+          source={wateringImg}
+          style={styles.image}
+          resizeMode="contain"
+        />
+
+        <Text style={styles.subtitle}>
+          Não esqueça mais de regar suas plantas.
+          Nós cuidamos de lembrar você sempre que precisar.
+        </Text>
+
+        <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.7}
+        >
+          <Feather
+            name="chevron-right"
+            style={styles.buttonIcon}
+          />
+        </TouchableOpacity>
+      </View>
     </SafeAreaView>
   );
 }
@@ -46,16 +53,23 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+
+  wrapper: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
+    paddingHorizontal: 20,
   },
 
   title: {
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
     marginTop: 45,
+    fontFamily: fonts.heading,
+    lineHeight: 34,
   },
 
   subtitle: {
@@ -63,6 +77,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
 
   image: {
@@ -79,8 +94,8 @@ const styles = StyleSheet.create({
     width: 56,
   },
 
-  buttonText: {
+  buttonIcon: {
+    fontSize: 32,
     color: colors.white,
-    fontSize: 24,
   }
 });

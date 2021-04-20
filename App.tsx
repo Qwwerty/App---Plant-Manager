14a6bfc1a38a9bd3b040b6e933bf741708253d23
DESKTOP-V5NHMSB\Rhalf <verdinhoraro2009@hotmail.com>
2021-04-20 11:00:16 -0300

    feat: tela de início

--- App.tsx
+++ App.tsx
@@ -1,13 +1,9 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Welcome } from './src/pages/Welcome';
 
 
 export default function App() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>
-        NLW#5
-      </Text>
-    </View>
+    <Welcome />
   );
 }
